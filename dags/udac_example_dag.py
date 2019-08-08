743cd6225da2a7a1044f85ff006632a1dca12570
Ronnald R Machado <rrmachado@id.uff.br>
2019-08-08 00:35:34 -0300

    First Commit

---
+++ dags/udac_example_dag.py
@@ -0,0 +1,123 @@
+'''
+This file
+implements a airflow Dag
+
+Author: Ronnald R. Machado
+'''
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators import StageToRedshiftOperator
+from airflow.operators import LoadFactOperator
+from airflow.operators import LoadDimensionOperator
+from airflow.operators import DataQualityOperator    
+from helpers import SqlQueries
+from airflow.operators.bash_operator import BashOperator
+
+
+default_args = {
+    'owner': 'ronnald',
+    'start_date': datetime.utcnow(),
+    'depends_on_past': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup' : False
+}
+
+dag = DAG('project5_1',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='@hourly')
+
+start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='stage_events',
+    dag=dag,
+    redshift_conn_id='redshift',
+    aws_credentials_id="aws_credentials",
+    table="staging_events",
+    s3_source="s3://udacity-dend/log_data",
+    json_paths="s3://udacity-dend/log_json_path.json",
+    file_type="JSON"
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='stage_songs',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_songs",
+    s3_source="s3://udacity-dend/song_data/A/",
+    json_paths="auto",
+    file_type="JSON"
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songplays",
+    query=SqlQueries.songplay_table_insert
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='users',
+    query=SqlQueries.user_table_insert,
+    mode='delete-load'
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='songs',
+    query=SqlQueries.song_table_insert,
+    mode='delete-load'
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="artists",
+    query=SqlQueries.artist_table_insert,
+    mode='delete-load'
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="time",
+    query=SqlQueries.time_table_insert,
+    mode='delete-load'
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    redshift_conn_id="redshift",
+    data_quality_query = "SELECT COUNT(*) FROM ",
+    tables=["staging_events","staging_songs",
+            "songplays","users",
+            "songs","artists","time"]
+)
+
+end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
+
+start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
+
+[stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+
+dim_tables = [load_user_dimension_table, load_song_dimension_table, 
+              load_artist_dimension_table, load_time_dimension_table]
+
+load_songplays_table >> dim_tables >> run_quality_checks
+
+run_quality_checks >> end_operator
---
+++ plugins/operators/data_quality.py
@@ -0,0 +1,39 @@
+'''
+Data quality
+'''
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class DataQualityOperator(BaseOperator):
+
+    ui_color = '#89DA59'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 tables="",
+                 data_quality_query="",
+                 *args, **kwargs):
+
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
+        self.data_quality_query = data_quality_query
+
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Running quality checks for {}".format(self.tables))
+        tables = self.tables
+        for table in tables:
+            quality_query = "{}{}".format(self.data_quality_query, table)
+            record = redshift.get_records(quality_query)
+            if ( int(record[0][0]) < 1):
+                raise ValueError(f"Data quality check failed in table: {table}")
+                self.log.info(f"Quality checks fail in table: {table}")
+            else:
+                self.log.info(f"{table} pass in data quality check")
+                
+        self.log.info(f"Success Data Quality checks")
+       
